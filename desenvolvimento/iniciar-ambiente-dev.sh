ce2b6d6ede2529dcfc571426b90be26530c452c9
Antônio "acdc" Jr <acdcjunior@gmail.com>
2017-03-30 13:41:42 -0300

    docker: novas configuracoes de dev

--- desenvolvimento/iniciar-ambiente-dev.sh
+++ desenvolvimento/iniciar-ambiente-dev.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Este docker-compose sobe um Apache+PHP na porta 80 local (http://127.0.0.1)
+# php7... na http://127.0.0.1:80/
+# couchdb na http://127.0.0.1:5984/_utils/
+# fauxton na http://127.0.0.1:8984/
 docker-compose up
 
 # Use o comando abaixo se nao quiser deixar uma janela aberta
