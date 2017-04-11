c9ec8d5b7bcee837248108507655ccbbd46379e1
Antônio "acdc" Jr <acdcjunior@gmail.com>
2017-04-11 20:02:42 -0300

    back-end: adicionar cors em /committers

--- back-end-node/routes/committers.js
+++ back-end-node/routes/committers.js
@@ -5,6 +5,9 @@ const router = express.Router();
 
 router.get('/', function(req, res, next) {
     Committer.findAll().then(committers => {
+        res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8080');
+        res.header('Access-Control-Allow-Methods', 'GET');
+        res.header('Access-Control-Allow-Headers', 'Content-Type');
         res.send(committers);
     });
 });
