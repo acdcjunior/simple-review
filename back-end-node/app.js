d6414db69b3b398974c689691ea3cfe51f621ee3
Antônio "acdc" Jr <acdcjunior@gmail.com>
2017-04-11 19:38:47 -0300

    front-end: jogar build na pasta do back-end

--- back-end-node/app.js
+++ back-end-node/app.js
@@ -21,7 +21,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', express.static(path.join(__dirname, 'front')));
+app.use('/index', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
