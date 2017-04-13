8a17afc895a40bd822022a53437626bb58da40cf
Antônio "acdc" Jr <acdcjunior@gmail.com>
2017-04-12 23:19:02 -0300

    docker: criar script pra subir tudo + remover arquivos antigos

--- desenvolvimento/iniciar-ambiente-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# php7... na http://127.0.0.1:80/
-# couchdb na http://127.0.0.1:5984/_utils/
-# fauxton na http://127.0.0.1:8984/
-docker-compose up
-
-# Use o comando abaixo se nao quiser deixar uma janela aberta
-# docker-compose up -d
---
+++ desenvolvimento/up.bat
@@ -0,0 +1,6 @@
+cd ../front-end
+npm install && npm run build
+cd ../desenvolvimento
+docker-compose up --build
+
+# docker-compose up -d
