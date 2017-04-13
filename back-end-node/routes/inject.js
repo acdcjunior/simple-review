4209ac4e61b03551ad4051e775a76cbaaf8e73d9
Antônio "acdc" Jr <acdcjunior@gmail.com>
2017-04-12 23:21:56 -0300

    back-end: inject remove headers de janela de diff

--- back-end-node/routes/inject.js
+++ back-end-node/routes/inject.js
@@ -2,7 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res) {
-    res.send('alert("hey2!");');
+    res.send(`
+        console.log('inject.js de code review carregado!');
+        $(() => {
+            // se eh URL de diff
+            if (/^\/\w+\/\w+\/commit\/.+$/.test(window.location.pathname)) {
+                // remove os headers
+                $('body > div.page-with-sidebar > div.content-wrapper.page-with-layout-nav > div.scrolling-tabs-container.sub-nav-scroll').remove();
+                $('body > div.page-with-sidebar > div.layout-nav').remove();
+                $('body > header').remove();
+            }
+        });
+    `);
 });
 
 module.exports = router;
