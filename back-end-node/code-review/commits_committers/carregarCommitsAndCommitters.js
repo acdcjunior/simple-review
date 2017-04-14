6c90df28d99ffbdd403f246686c2f76ccc7b2b6e
Antônio "acdc" Jr <acdcjunior@gmail.com>
2017-04-14 15:26:39 -0300

    back-end: movendo carregarCommitters para module separada

--- back-end-node/code-review/commits_committers/carregarCommitsAndCommitters.js
+++ back-end-node/code-review/commits_committers/carregarCommitsAndCommitters.js
@@ -1,84 +1,13 @@
 const Utils = require('../util/Utils');
 
-const sesol2Repository = require('../domain/Sesol2Repository');
-const Committer = require('../domain/Committer');
-const GitLab = require('../domain/GitLab');
-
+const carregarCommitters = require('./carregarCommitters');
 const carregarCommits = require('./carregarCommits');
-
-function getCommittersDosUltimosCommits() {
-    return GitLab.getCommits().then(commits => {
-        console.log(`Processando COMMITTERS...`);
-
-        console.log(`\tCarregrando committers de ${commits.length} commits...`);
-        let committersHash = {};
-        commits.forEach(commit => {
-            committersHash[commit.author_email] = true;
-        });
-        const committers = Object.keys(committersHash);
-
-        return Promise.resolve(committers);
-    });
-}
-
-function avisarSeUsuarioNaoEncontradoDeManeiraUnica(committerUser, committerEmail) {
-    if (committerUser.length !== 1) {
-
-        for (let i = 0; i < 50; i++) {
-            console.log('USUARIO NAO ENCONTRADO DE MANEIRA UNICA');
-        }
-        console.log(committerEmail, committerUser);
-        Utils.printBar(2);
-    }
-    return committerUser[0];
-}
-
-function carregarCommitters() {
-
-    return new Promise(resolve => {
-        getCommittersDosUltimosCommits().then(committersDosUltimosCommits => {
-            let promisesDeCommittersInseridos = [];
-
-            console.log(`\tInserindo, se necessario, ultimos ${committersDosUltimosCommits.length} committers...`);
-            committersDosUltimosCommits.forEach(committerEmail => {
-                promisesDeCommittersInseridos.push(
-
-                    GitLab.getUser(committerEmail).then(committerUser => {
-
-                        let userDoGitlab = avisarSeUsuarioNaoEncontradoDeManeiraUnica(committerUser, committerEmail);
-                        return sesol2Repository.insertIfNotExists(
-                            new Committer(committerEmail, userDoGitlab.name, userDoGitlab.avatar_url)
-                        );
-
-                    })
-
-                );
-            });
-
-            Promise.all(promisesDeCommittersInseridos).then(resultadosDasPromises => {
-                let jahExistiam = 0;
-                resultadosDasPromises.forEach(resultadoDePromise => {
-                    if (!resultadoDePromise) {
-                        jahExistiam++;
-                    } else {
-                        console.log('\t\t' + resultadoDePromise);
-                    }
-                });
-                console.log(`\tJah existiam: ${jahExistiam}`);
-                console.info("Fim da carga de committers!");
-
-                Utils.printBar();
-                resolve();
-            });
-        })
-    });
-}
 
 function carregarCommitsAndCommitters() {
     console.log('Iniciando carga de committers e commits...');
     Utils.printBar();
 
-    return  carregarCommitters().then(() => {
+    return carregarCommitters().then(() => {
         carregarCommits();
     });
 }
---
+++ back-end-node/code-review/commits_committers/carregarCommitters.js
@@ -0,0 +1,76 @@
+const Utils = require('../util/Utils');
+const sesol2Repository = require('../domain/Sesol2Repository');
+const Committer = require('../domain/Committer');
+const GitLab = require('../domain/GitLab');
+
+function getCommittersDosUltimosCommits() {
+    return GitLab.getCommits().then(commits => {
+        console.log(`Processando COMMITTERS...`);
+
+        console.log(`\tCarregrando committers de ${commits.length} commits...`);
+        let committersHash = {};
+        commits.forEach(commit => {
+            committersHash[commit.author_email] = true;
+        });
+        const committers = Object.keys(committersHash);
+
+        return Promise.resolve(committers);
+    });
+}
+
+function avisarSeUsuarioNaoEncontradoDeManeiraUnica(committerUser, committerEmail) {
+    if (committerUser.length !== 1) {
+
+        for (let i = 0; i < 50; i++) {
+            console.log('USUARIO NAO ENCONTRADO DE MANEIRA UNICA');
+        }
+        console.log(committerEmail, committerUser);
+        Utils.printBar(2);
+    }
+    return committerUser[0];
+}
+
+function carregarCommitters() {
+
+    return new Promise(resolve => {
+
+        getCommittersDosUltimosCommits().then(committersDosUltimosCommits => {
+            let promisesDeCommittersInseridos = [];
+
+            console.log(`\tInserindo, se necessario, ultimos ${committersDosUltimosCommits.length} committers...`);
+            committersDosUltimosCommits.forEach(committerEmail => {
+                promisesDeCommittersInseridos.push(
+
+                    GitLab.getUser(committerEmail).then(committerUser => {
+
+                        let userDoGitlab = avisarSeUsuarioNaoEncontradoDeManeiraUnica(committerUser, committerEmail);
+                        return sesol2Repository.insertIfNotExists(
+                            new Committer(committerEmail, userDoGitlab.name, userDoGitlab.avatar_url)
+                        );
+
+                    })
+
+                );
+            });
+
+            Promise.all(promisesDeCommittersInseridos).then(resultadosDasPromises => {
+                let jahExistiam = 0;
+                resultadosDasPromises.forEach(resultadoDePromise => {
+                    if (!resultadoDePromise) {
+                        jahExistiam++;
+                    } else {
+                        console.log('\t\t' + resultadoDePromise);
+                    }
+                });
+                console.log(`\tJah existiam: ${jahExistiam}`);
+                console.info("Fim da carga de committers!");
+
+                Utils.printBar();
+                resolve();
+            });
+        })
+
+    });
+}
+
+module.exports = carregarCommitters;
