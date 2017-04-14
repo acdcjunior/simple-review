ad16577530237d920c11519ba0255f3ab17c15a7
Antônio "acdc" Jr <acdcjunior@gmail.com>
2017-04-14 18:37:40 -0300

    back-end: remover indice nao usado

--- back-end-node/code-review/infra/couch-install/addCommitsIndexes.js
+++ back-end-node/code-review/infra/couch-install/addCommitsIndexes.js
@@ -10,28 +10,6 @@ function addCommitsIndexes() {
             }
         }
     ).then(() => {
-
-        return criarView(
-            'commits_revisados_sim_index',
-            function (commit) {
-                if (commit.type === 'commit') {
-                    var revisoresPendentes = commit.revisores.length;
-                    for (var i = 0; i < commit.revisoes.length; i++) {
-                        var revisor = commit.revisoes[i].revisor;
-                        if (commit.revisores.indexOf(revisor) !== -1) {
-                            revisoresPendentes--;
-                        }
-                    }
-                    var commitRevisado = revisoresPendentes <= 0;
-                    if (commitRevisado) {
-                        emit(commit.created_at);
-                    }
-                }
-            }
-        );
-
-    }).then(() => {
-
         return criarView(
             'commits_revisados_nao_index',
             function (commit) {
@@ -50,7 +28,6 @@ function addCommitsIndexes() {
                 }
             }
         );
-
     });
 
 }
