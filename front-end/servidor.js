556c4f4ef2d16894883b28c997fb34d75bd3ef00
Antônio "acdc" Jr <acdcjunior@gmail.com>
2017-04-18 19:35:21 -0300

    front-end: servir arquivos via express

---
+++ front-end/servidor.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const router = express.Router();
+
+// simple logger for this router's requests
+// all requests to this router will first hit this middleware
+router.use(function(req, res, next) {
+    console.log('%s %s %s', req.method, req.url, req.path);
+    next();
+});
+
+app.use(`/code-review`, express.static(path.join(__dirname, 'dist')));
+
+app.listen(8080);
