fd7620ce1be50f89de7640108192f13232ed67eb
Antônio "acdc" Jr <acdcjunior@gmail.com>
2017-04-19 16:21:02 -0300

    back-end: parametrizar perPage

--- back-end-node/code-review/domain/GitLabConfig.js
+++ back-end-node/code-review/domain/GitLabConfig.js
@@ -7,8 +7,9 @@ const privateToken = process.env.GITLAB_HOST_PRIVATE_TOKEN || 'M3_6_x-z3HQEPc4Z4
 
 class GitLabConfig {
 
-    static projectsUrl () {
-        return `http://${host}/api/v4/projects/${projectId}/repository/commits/?ref_name=${projectBranch}&per_page=100`;
+    static projectsUrl (perPage) {
+        let perPageEfetivo = perPage || 10;
+        return `http://${host}/api/v4/projects/${projectId}/repository/commits/?ref_name=${projectBranch}&per_page=${perPageEfetivo}`;
     }
     static usersUrl (committerEmail) {
         const emailCorrigido = Committer.corrigirEmail(committerEmail);
