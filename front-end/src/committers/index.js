66e94ab140e79c2555afd8a55b13634c4a7c1466
Antônio "acdc" Jr <acdcjunior@gmail.com>
2017-04-27 19:44:24 -0300

    front: incluir usuario 'nao serah revisado'

--- front-end/src/committers/index.js
+++ front-end/src/committers/index.js
@@ -1,10 +1,13 @@
 const imgInterrogacao = require('../assets/question_mark.png');
+const imgMerge = require('../assets/merge.png');
 import {CommitterEntity} from "../servicos/CommitterService";
+
+const naoTerahRevisor = {email: 'nao-terah-revisor@srv-codereview.tcu.gov.br', username: 'nao-terah-revisor', name: 'Erro ao obter committers.', sexo: "m", avatar_url: imgMerge};
 
 const committers = {
   commiterLogado: undefined,
   committers: {
-    'nada@erro.com': {email: 'nada@erro.com', username: 'erro', name: 'Erro ao obter committers.', avatar_url: imgInterrogacao}
+    'nada@erro.com': {email: 'nada@erro.com', username: 'erro', name: 'Erro ao obter committers.', sexo: "m", avatar_url: imgInterrogacao}
   },
   committerEntity: function (email) {
       return new CommitterEntity(committers.committers[email]);
@@ -18,6 +21,7 @@ window.$.ajax({
   timeout: 3000, // sets timeout to 3 seconds
   success: receivedCommitters => {
     committers.committers = {};
+    committers.committers[naoTerahRevisor.email] = naoTerahRevisor;
     receivedCommitters.forEach(receivedCommitter => {
       committers.committers[receivedCommitter.email] = receivedCommitter
     })
