fa503b8f7dd888b6c5725cd8b581d4fd87805970
Antônio "acdc" Jr <acdcjunior@gmail.com>
2017-04-30 16:39:08 -0300

    fix(back): assinatura da funcao rest mudou

--- back-end-node/routes/jenkins.js
+++ back-end-node/routes/jenkins.js
@@ -1,11 +1,12 @@
-const rest = require('../build/infra/rest').rest;
-const addCors = require('./addCors');
-
 const express = require('express');
 const router = express.Router();
 
+const addCors = require('./addCors');
+//noinspection JSUnresolvedVariable
+const Rest = require('../build/infra/rest').Rest;
+
 router.get('/pipeline', function(req, res) {
-    rest('GET', 'http://srv-ic-master:8089/view/Sesol-2/job/sagas2.pipeline/api/json?pretty=true').then(data => {
+    Rest.get('http://srv-ic-master:8089/view/Sesol-2/job/sagas2.pipeline/api/json?pretty=true').then(data => {
         addCors(req, res);
         res.send(data);
     });
